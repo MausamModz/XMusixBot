6b141be05aa813f1966dddcf2b54957ff3995397
MausamModz <166141219+MausamModz@users.noreply.github.com>
2025-02-27 09:21:24 +0545

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/MausamModz/XMusic",
+    "https://github.com/MausamModz/XMusixBot",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
